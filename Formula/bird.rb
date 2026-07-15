047bb66f197d31e25ac4a907c0c52ac397cf1c7d
bird-release <bird-release@bird.com>
2026-07-15 15:09:34 +0000

    bird 0.8.7

--- Formula/bird.rb
+++ Formula/bird.rb
@@ -5,21 +5,21 @@
 class Bird < Formula
   desc "Operate the Bird platform from a shell, script, or AI agent"
   homepage "https://bird.com/cli"
-  version "0.8.6"
+  version "0.8.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://cli.platform.bird.com/releases/v0.8.6/bird-darwin-amd64.tar.gz"
-      sha256 "eb3fdc61cacc1c8fc94e98e7a6310d7cb430843bbe95ead2f6cf1f6cd1c0168a"
+      url "https://cli.platform.bird.com/releases/v0.8.7/bird-darwin-amd64.tar.gz"
+      sha256 "b39429ed78eea0d826e687cca8acc95294ae103130302ca9ac967b900e178de7"
 
       define_method(:install) do
         bin.install "bird"
       end
     end
     if Hardware::CPU.arm?
-      url "https://cli.platform.bird.com/releases/v0.8.6/bird-darwin-arm64.tar.gz"
-      sha256 "81f7a48635be42701baf8377df6499327272aeb99f843785e1bc0c44771651f1"
+      url "https://cli.platform.bird.com/releases/v0.8.7/bird-darwin-arm64.tar.gz"
+      sha256 "d3b3588d66121f64f3952e0cb162fdbb34ab163378538a5841a1e543020e24e0"
 
       define_method(:install) do
         bin.install "bird"
@@ -29,16 +29,16 @@ class Bird < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://cli.platform.bird.com/releases/v0.8.6/bird-linux-amd64.tar.gz"
-      sha256 "633669eb10f1d25a3b3b2b896d015e06cee4fb331b73733223316259d65b0442"
+      url "https://cli.platform.bird.com/releases/v0.8.7/bird-linux-amd64.tar.gz"
+      sha256 "42eab0aefbe7027ee3f56685a5abbee31209af7f1afefcafbe5d6919af658fca"
 
       define_method(:install) do
         bin.install "bird"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://cli.platform.bird.com/releases/v0.8.6/bird-linux-arm64.tar.gz"
-      sha256 "d87741453566026cdbe8380cf2af01527fc1db2324978f5b8146214603a43208"
+      url "https://cli.platform.bird.com/releases/v0.8.7/bird-linux-arm64.tar.gz"
+      sha256 "45d5e6e9e186c990e09269cb007806445d76ddec5e93c1296db60888ed170875"
 
       define_method(:install) do
         bin.install "bird"
